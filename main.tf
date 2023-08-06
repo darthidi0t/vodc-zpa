fa8e8dc536e52a6424d9a13b7c8d206bfd7bf974
darthidi0t <60817501+darthidi0t@users.noreply.github.com>
2023-08-06 17:25:49 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -14,16 +14,16 @@ terraform {
 
 provider "aws" {
   region     = "ap-south-1"
-  access_key = "AKIA6MKCUR2FHYVWFHTM"
-  secret_key = "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"
+  access_key = "xxxxxxx"
+  secret_key = "xxxxxxx"
 
 }
 
 # Submit provider variables
 
 provider "zpa" {
-  zpa_client_id         = "Mjg4MjYwNTc1MTQwNTExOTAyLTJkODQ4MDNjLTlhZGYtNDEwMC1iZmY5LWE2NWQwZGZlOTA0Yg=="
-  zpa_client_secret     = "T_#zT;j]r:P@_WT'fdch48TA#/9`#2q`"
-  zpa_customer_id       = "288260575140511744"
+  zpa_client_id         = "xxxxx"
+  zpa_client_secret     = "xxxxx"
+  zpa_customer_id       = "xxxxx"
 }
 
